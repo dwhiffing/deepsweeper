70019b9a1b77edc8c6784bb92b41120f00638ecb
Daniel Whiffing <danielwhiffing@gmail.com>
2025-04-06 11:23:51 -0400

    Adds outlines

--- src/prefabs/Cube.tsx
+++ src/prefabs/Cube.tsx
@@ -1,8 +1,5 @@
-import * as THREE from 'three'
-import { useBox } from '@react-three/cannon'
-import { LineSegments } from 'three'
-import { useEffect, useRef } from 'react'
-import { Triplet } from '@react-three/cannon'
+import { useBox, Triplet } from '@react-three/cannon'
+import { Outlines } from '@react-three/drei'
 
 export const Cube = (props: {
   position: Triplet
@@ -11,6 +8,7 @@ export const Cube = (props: {
   onCollide: () => void
 }) => {
   const { position, isSolid = true } = props
+
   const [cubeRef] = useBox(() => ({
     mass: 1,
     args: [0.5, 0.5, 0.5],
@@ -21,27 +19,18 @@ export const Cube = (props: {
     onCollide: props.onCollide,
   }))
 
-  const dashedRef = useRef<LineSegments>(null!)
-
-  useEffect(() => {
-    if (dashedRef.current) {
-      dashedRef.current.computeLineDistances()
-    }
-  }, [])
-
-  if (!isSolid) {
-    return (
-      <lineSegments ref={dashedRef} position={position}>
-        <edgesGeometry args={[new THREE.BoxGeometry(0.5, 0.5, 0.5)]} />
-        <lineDashedMaterial color="white" dashSize={0.1} gapSize={0.1} />
-      </lineSegments>
-    )
-  }
-
   return (
     <mesh ref={cubeRef} castShadow>
       <boxGeometry args={[0.5, 0.5, 0.5]} />
-      <meshLambertMaterial color={props.isMine ? '#ff0000' : '#0077ff'} />
+      <meshLambertMaterial
+        opacity={isSolid ? 1 : 0}
+        transparent={!isSolid}
+        depthTest
+        color={props.isMine ? '#ff0000' : '#0077ff'}
+      />
+      {!isSolid && (
+        <Outlines thickness={2} color="#fff" transparent opacity={0.3} />
+      )}
     </mesh>
   )
 }
