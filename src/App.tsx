ae06f47e9a5ce1ee81308d29e6b4519389f45138
Daniel Whiffing <danielwhiffing@gmail.com>
2025-04-06 14:20:13 -0400

    Adds better menu fade

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,58 @@ import { DefaultScene } from './scene/DefaultScene'
 import './index.css'
 import { useEffect, useState } from 'react'
 
-const DEBUG = false
+export const DEBUG = false
+const DUR = 1000
 
 export default function App() {
   const [gameStarted, setGameStarted] = useState(DEBUG)
-  const [fade, setFade] = useState(false)
+  const [gameFade, setGameFade] = useState(false)
+  const [menuFade, setMenuFade] = useState(false)
   useEffect(() => {
-    if (gameStarted) setTimeout(() => setFade(true), 1000)
+    setTimeout(() => setGameFade(gameStarted), DUR)
   }, [gameStarted])
 
   return gameStarted ? (
     <>
       <UI>
         <div
-          className="transition-all inset-0 fixed z-20 duration-1000"
-          style={{ backgroundColor: fade ? 'transparent' : '#001' }}
+          className="transition-all inset-0 fixed z-20"
+          style={{
+            backgroundColor: gameFade ? 'transparent' : '#001',
+            transitionDuration: `${gameFade ? DUR : DUR / 2}ms`,
+          }}
         />
         <Crosshair />
       </UI>
       <Canvas style={{ backgroundColor: '#001' }}>
-        <DefaultScene onGameOver={() => setGameStarted(false)} />
+        <DefaultScene
+          onGameOver={() => {
+            setGameFade(false)
+
+            setTimeout(() => {
+              setGameStarted(false)
+              setTimeout(() => setMenuFade(false), DUR / 2)
+            }, DUR)
+          }}
+        />
       </Canvas>
     </>
   ) : (
     <div className="flex justify-center items-center h-screen">
+      <div
+        className="transition-all inset-0 fixed z-20"
+        style={{
+          transitionDuration: `${DUR}ms`,
+          backgroundColor: menuFade ? '#001' : 'transparent',
+          pointerEvents: menuFade ? 'auto' : 'none',
+        }}
+      />
       <button
         className="bg-white border rounded-md px-4 py-2 cursor-pointer"
-        onClick={() => setGameStarted(true)}
+        onClick={() => {
+          setMenuFade(true)
+          setTimeout(() => setGameStarted(true), DUR)
+        }}
       >
         Start Game
       </button>
